3c38b8b33b1491e736b52ec71a5920a0c0b8ddc0
bartstachurski <bart.stachurski@gmail.com>
2019-06-26 19:55:55 -0400

    create action to display name of query variable in all caps and provide message if name begins with a - create a guessing game route controller action

--- app/controllers/api/name_controller.rb
+++ app/controllers/api/name_controller.rb
@@ -1,7 +1,24 @@
 class Api::NameController < ApplicationController
 
   def name_in_caps
-
+    p params
+    @name = params["name"].upcase
+    if @name.chr.downcase == "a"
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    end
     render 'name_in_caps.json.jb'
+  end
+
+  def guess_a_number
+    number = rand(0..100)
+    p number
+    if params["guess"].to_i == number
+      @message = "Congrats, you guessed correctly!"
+    elsif params["guess"].to_i < number
+      @message = "Oops, too low. Guess a higher number."
+    else params["guess"].to_i > number
+      @message = "Oops, too high. Guess a lower number."
+    end
+    render 'guess_a_number.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/name" => "name#name_in_caps"
+    get "/guess_a_number" => "name#guess_a_number"
   end
 end
